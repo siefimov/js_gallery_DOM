fc53e9ae2fa0744294514bed859d59929f49487c
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-07-12 19:58:09 +0300

    make build

---
+++ index.9625d23c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelectorAll(".list-item").forEach(function(t){t.addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a").href;e.src=r})});
+//# sourceMappingURL=index.9625d23c.js.map
